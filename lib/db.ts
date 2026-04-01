9014c15c6e530902789c7c9d246b4ea1072de8d7
Rakib-dhali~ <rakibdhali343@gmail.com>
2026-04-01 21:58:59 +0600

    database connecting

---
+++ lib/db.ts
@@ -0,0 +1,37 @@
+import mongoose, { Connection } from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("please define mongoUri in env variables");
+}
+
+const cached = global.mongoose;
+if (!cached) {
+  global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB(): Promise<Connection | null> {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = { bufferCommands: false };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+}
+
+export { connectDB };
